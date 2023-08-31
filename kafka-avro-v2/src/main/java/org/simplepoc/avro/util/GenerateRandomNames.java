cfccaac096b4740735d6370d769caa7c2c48d5bd
adriangonc <adriangonc@yahoo.com.br>
2023-08-31 17:37:31 -0300

    Method to generate random names

---
+++ kafka-avro-v2/src/main/java/org/simplepoc/avro/util/GenerateRandomNames.java
@@ -0,0 +1,17 @@
+package org.simplepoc.avro.util;
+
+import java.util.Random;
+
+public class GenerateRandomNames {
+
+    private static final String[] FIRST_NAMES = {"Ana", "Sara", "Adriano", "John", "Maria", "Ripley", "Leon", "Jose", "Jennifer", "Artoris", "Jim"};
+    private static final String[] LAST_NAMES = {"Souza", "Raynor", "Lawrence", "Williams", "Costa", "Kruger", "Scot Kenedy", "Wilson", "Kerrigan", "Taylor", "Anderson", "Thomas", "Jason"}
+
+    public static String generateRandomNames(){
+        Random r = new Random();
+        String firstName = FIRST_NAMES[r.nextInt(FIRST_NAMES.length)];
+        String lastName = LAST_NAMES[r.nextInt(LAST_NAMES.length)];
+
+        return firstName.concat(lastName);
+    }
+}
